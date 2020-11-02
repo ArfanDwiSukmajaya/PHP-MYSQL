1d7b0f05bf1845af78110a71263e175064dc18c0
Arfan Dwi Sukmajaya <jayaarfan92@gmail.com>
2020-11-02 09:05:22 +0700

    menambahkan fitur cookie

--- login.php
+++ login.php
@@ -1,12 +1,28 @@
 <?php 
   session_start();
+  
+  include 'functions.php';
+
+  // cek cookie
+  if(isset($_COOKIE["id"]) && isset($_COOKIE["key"])){
+    $id = $_COOKIE["id"];
+    $key = $_COOKIE["key"];
+
+    // ambil username berdasarkan id
+    $result = mysqli_query($conn, "SELECT username FROM user WHERE id = $id");
+    $row = mysqli_fetch_assoc($result);
+
+    // cek cookie dan username berdasarkan
+    if($key ===  hash('sha256',$row['username'])){
+      $_SESSION["login"] = true;
+    }
+
+  }
 
   if( isset($_SESSION["login"])){
     header("Location:index.php");
     exit;
   }
-
-  include 'functions.php';
 
   if(isset($_POST["login"])){
     $username = $_POST["username"];
@@ -20,8 +36,15 @@
       $row = mysqli_fetch_assoc($result);
       $_SESSION["username"] = $row["username"];
       if(password_verify($password, $row['password'])){
-        // Set session
+        // Set session 
         $_SESSION["login"] = true;
+
+        // cek remember me
+        if(isset($_POST["remember"])){
+          // buat cookie
+          setcookie('id',$row["id"], time() + 60 );
+          setcookie('key', hash('sha256' ,$row["username"]));
+        }
         
         header("Location:index.php"); 
         exit;
@@ -59,6 +82,10 @@
       <li>
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
+      </li>
+      <li>
+        <input type="checkbox" name="remember" id="remember">
+        <label for="remember">Remember Me</label>
       </li>
       <li>
         <button type="submit" name="login">Login</button>
